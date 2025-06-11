aa8099bd60153732b4676974e63df64486aee567
LuizMoura <luizrmoura1001@gmail.com>
2025-06-11 16:55:19 -0300

    edit config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Jira
 BASE_URL        = os.getenv("BASE_URL")
 EMAIL           = os.getenv("EMAIL")
 API_TOKEN_JIRA  = os.getenv("API_TOKEN_JIRA")
 
-# OpenAI (ou outras)
+# OpenAI
 OPENAI_API_KEY  = os.getenv("OPENAI_API_KEY")
