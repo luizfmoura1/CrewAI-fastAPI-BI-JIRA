800de59f50009a6a71f8bcfff23d5c4e9f492b1f
LuizMoura <luizrmoura1001@gmail.com>
2025-06-27 17:17:05 -0300

    base funcional

--- src/utils/custom_llm.py
+++ src/utils/custom_llm.py
@@ -25,7 +25,7 @@ class ChatDatabricks(SimpleChatModel):
     endpoint_url: str
     token: str
     temperature: float = 0.7
-    max_tokens: int = 10000 # Aumentei um pouco para análises mais longas
+    max_tokens: int = 10000 
 
     def _call(
         self,
